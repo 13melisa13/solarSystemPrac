5757b7aed077716c5461216230253692345400a0
13melisa13 <114099749+13melisa13@users.noreply.github.com>
2022-11-19 13:58:40 +0300

    Add files via upload

---
+++ reg.js
@@ -0,0 +1,10 @@
+
+function showMessageLogUp(){
+    let ans= prompt("Желаете пройти регистрацию на сайте?");
+    if (ans === "Да") {
+        alert("Круто!");
+    } else {
+        alert("Попробуйте еще раз!");
+    }
+}
+showMessageLogUp();
