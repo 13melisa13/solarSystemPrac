c23df269e4d73aed12c4ceda4e4cbacd75455d36
13melisa13 <114099749+13melisa13@users.noreply.github.com>
2022-12-03 23:46:48 +0300

    Update link.js

--- link.js
+++ link.js
@@ -1,5 +1,5 @@
 let links = document.querySelectorAll("a");
 for (const link of links) {
-    if ((link.href).includes("http"))
+    if (!(link.href).includes("13melisa13.github.io/solarSystemPrac"))
         link.style.color = "red";    
 }
