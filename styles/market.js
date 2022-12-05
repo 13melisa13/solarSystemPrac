c098a0059622896498d859a529c0b0deb4abefe7
Adeline1301 <isaeva.adelya2016@ya.ru>
2022-12-05 13:29:42 +0300

    5

--- styles/market.js
+++ styles/market.js
@@ -9,7 +9,7 @@ let basket = market.querySelector(".basket");
       
       document.addEventListener('mousemove', onMouseMove);
       document.addEventListener('mouseup', onMouseUp);
-
+      let currentDropable = null;
       function onMouseMove(event) {
         let top =  product.offsetHeight * (-[...board.children].indexOf(product));
         let x = event.clientX - shiftX - market.getBoundingClientRect().left;
@@ -17,24 +17,58 @@ let basket = market.querySelector(".basket");
         product.style.zIndex = "100";
         product.style.cursor = "grabbing";
         document.documentElement.style.setProperty('--content', "#e0ae3a");
-        
+        let lastX = product.style.left;
+        let lastY = product.style.top;
         if (x < 0) {
           x = 0;
+          y = lastY;
         }
         if (y  < top) {
           y = top;
+          x = lastX;
         }
         let rightEdge = market.offsetWidth - product.offsetWidth;
         let bottomEdge = market.offsetHeight - 
           product.offsetHeight * (1 + [...board.children].indexOf(product));
         if (x > rightEdge) {
           x = rightEdge;
+          y = lastY;
         }
         if (y > bottomEdge) {
           y = bottomEdge;
+          x = lastX;
         }
         product.style.left = x + 'px';
         product.style.top = y + 'px';
+
+        // product.hidden = true;
+         let basketFind = null;//document.elementFromPoint(event.clientX,event.clientY);
+        // product.hidden = false;
+
+        if (event.clientX <= basket.getBoundingClientRect().right &&
+        event.clientX >= basket.getBoundingClientRect().left &&
+        event.clientY >= basket.getBoundingClientRect().top &&
+        event.clientY <= basket.getBoundingClientRect().bottom){
+          basketFind = basket;
+          document.documentElement.style.setProperty('--content', "green");
+        }
+            
+        if (currentDropable != basketFind){
+            if (currentDropable){
+              document.documentElement.style.setProperty('--content', "red");  
+              basket.textContent = 
+              +market.querySelector(".basket").textContent - +product.textContent;
+              //alert(market.querySelector(".basket").textContent);
+            }
+            currentDropable = basketFind;
+            if (currentDropable){
+              document.documentElement.style.setProperty('--content', "green");
+              basket.textContent = +basket.textContent + +product.textContent;
+              //alert(basket.textContent);
+            }
+        }
+
+        
       }
 
       function onMouseUp() {
